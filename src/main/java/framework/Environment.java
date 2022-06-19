667d630d30365425c7e91f1b885bbef6647a548b
net_creature <agustinmediotti@gmail.com>
2022-06-19 01:13:56 -0300

    first setup

---
+++ src/main/java/framework/Environment.java
@@ -0,0 +1,63 @@
+package framework;
+
+import utils.LoggerManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Environment {
+    private Properties properties;
+    private static final LoggerManager log = LoggerManager.getInstance();
+    private static Environment instance;
+
+    public Environment() {
+        initialize();
+    }
+
+    public static Environment getInstance() {
+        if(instance == null) {
+            instance = new Environment();
+        }
+        return instance;
+    }
+
+    private void initialize() {
+        log.info("Reading Properties File...");
+        properties = new Properties();
+        try {
+            File file = new File("gradle.properties");
+            FileReader fileReader = new FileReader(file);
+            properties.load(fileReader);
+            fileReader.close();
+        } catch (IOException e) {
+            log.error("Unable to read properties file");
+        }
+    }
+
+    public String getEnvironmentSetting(String setting) {
+        return (String) getInstance().properties.get(setting);
+    }
+
+    public String getBaseURL() {
+        return getEnvironmentSetting("baseURL");
+    }
+
+    public String getUserName() {
+        return getEnvironmentSetting("userName");
+    }
+
+    public String getPassword() {
+        return getEnvironmentSetting("password");
+    }
+
+    public String getBasePath() {
+        return getEnvironmentSetting("basePath");
+    }
+
+    public String getProjectEndPoint() {
+        return getEnvironmentSetting("projectEndPoint");
+    }
+}
